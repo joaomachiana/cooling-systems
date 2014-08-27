c1856f8c7aa665afc039ec8ee2bd7ddeddf00248
joaomachiana <joaomachiana@gmail.com>
2014-08-27 16:37:12 +0200

    removing cakephp

--- webapp/app/Model/ClientIdentification.php
+++
@@ -1,9 +0,0 @@
-<?php
-App::uses('AppModel', 'Model');
-/**
- * ClientsIdentificationType Model
- *
- */
-class ClientsIdentificationType extends AppModel {
-
-}
--- webapp/app/Model/DemographicIdentification.php
+++
@@ -1,9 +0,0 @@
-<?php
-App::uses('AppModel', 'Model');
-/**
- * IdentificationTypesPersonalInfo Model
- *
- */
-class IdentificationTypesPersonalInfo extends AppModel {
-
-}
